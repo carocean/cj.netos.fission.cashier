b6757d0e199ef7a2931c10af8d7511c4054b8eda
cj <cj@ss.com>
2021-02-28 13:26:47 +0800

    1.0.0

--- cj.netos.fission.program/src/cj/netos/fission/AbstractService.java
+++ cj.netos.fission.program/src/cj/netos/fission/AbstractService.java
@@ -1,7 +1,9 @@
 package cj.netos.fission;
 
+import cj.lns.chip.sos.cube.framework.CubeConfig;
 import cj.lns.chip.sos.cube.framework.ICube;
 import cj.lns.chip.sos.disk.INetDisk;
+import cj.studio.ecm.CJSystem;
 import cj.studio.ecm.annotation.CjServiceRef;
 
 public class AbstractService {
@@ -13,6 +15,15 @@ public class AbstractService {
         return home;
     }
     public ICube cube(String name){
-        return disk.cube(name);
+        try {
+            return disk.cube(name);
+        } catch (Exception e) {
+            if (!disk.existsCube(name)) {
+                CubeConfig cubeConfig = new CubeConfig();
+                disk.createCube(name,cubeConfig);
+                CJSystem.logging().info(getClass(),String.format("网盘已创建:%s",name));
+            }
+            return disk.cube(name);
+        }
     }
 }
--- cj.netos.fission.program/src/cj/netos/fission/chatroom/ChatroomService.java
+++ cj.netos.fission.program/src/cj/netos/fission/chatroom/ChatroomService.java
@@ -46,14 +46,17 @@ public class ChatroomService extends AbstractService implements IChatroomService
         if (chatroom == null) {
             Person payerPerson = personService.get(payer);
             chatroom = createChatroom(payerFull, roomId, payerPerson);
+            addMember(chatroom,payerFull,payerPerson,"creator");
+            return;
         }
+
         if (existsMember(chatroom, payeeFull)) {
             return;
         }
         //添加成员
 
         Person payeePerson = personService.get(payee);
-        addMember(chatroom, payeeFull, payeePerson);
+        addMember(chatroom, payeeFull, payeePerson,"user");
         PayRecord record = payRecordService.getRecord(recordSn);
         pushAddMemberEvent(chatroom, payeeFull, payeePerson, record);
     }
@@ -77,19 +80,19 @@ public class ChatroomService extends AbstractService implements IChatroomService
         rabbitMQProducer.publish("jobCenter", properties, body);
     }
 
-    private void addMember(Chatroom chatroom, String payeeFull, Person payeePerson) {
+    private void addMember(Chatroom chatroom, String personFull, Person personInfo,String actor) {
         RoomMember member = new RoomMember();
-        member.setActor("user");
+        member.setActor(actor);
         member.setAtime(System.currentTimeMillis());
         member.setFlag(0);
-        member.setNickName(payeePerson.getNickName());
-        member.setPerson(payeeFull);
+        member.setNickName(personInfo.getNickName());
+        member.setPerson(personFull);
         member.setRoom(chatroom.getRoom());
         cube(chatroom.getCreator()).saveDoc(_COL_MEMBER, new TupleDocument<>(member));
     }
 
     private boolean existsMember(Chatroom chatroom, String payeeFull) {
-        return cube(chatroom.getCreator()).tupleCount(_COL_ROOM, String.format("{'tuple.room':'%s','tuple.person':'%s'}", chatroom.getRoom(), payeeFull)) > 0;
+        return cube(chatroom.getCreator()).tupleCount(_COL_MEMBER, String.format("{'tuple.room':'%s','tuple.person':'%s'}", chatroom.getRoom(), payeeFull)) > 0;
     }
 
     private Chatroom createChatroom(String payerFull, String roomId, Person payerPerson) {
@@ -101,7 +104,6 @@ public class ChatroomService extends AbstractService implements IChatroomService
         chatroom.setRoom(roomId);
         chatroom.setTitle(String.format("裂变游戏·交个朋友-%s", payerPerson.getNickName()));
         cube(payerFull).saveDoc(_COL_ROOM, new TupleDocument<>(chatroom));
-        addMember(chatroom,payerFull,payerPerson);
         return chatroom;
     }
 
